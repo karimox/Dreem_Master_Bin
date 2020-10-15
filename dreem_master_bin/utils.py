6e25f458a9f13a429986d159ea991e6b64402a3d
karimox <karim@dreem.com>
2020-10-15 23:11:19 +0200

    correction

--- dreem_master_bin/utils.py
+++ dreem_master_bin/utils.py
@@ -13,3 +13,20 @@ def datetime_to_nightsec(d_time):
 
     except:
         return float('nan')
+
+
+def generate_timestamps_signals(signals, fs, start_time=None):
+    """
+    generate time array for a signal
+    """
+    if start_time:
+        start_time_sec = datetime_to_nightsec(start_time)
+        if start_time_sec > 3600 * 16:
+            start_time_sec -= 24 * 3600
+    else:
+        start_time_sec = 0
+
+    timestamps = np.arange(0, len(signals)) / fs  # time in sec
+    timestamps += start_time_sec
+
+    return timestamps
